480cd1f43af7702dc2057bcbbf549e7eeefc75e9
Naeem Sani <muhdnaeem95@gmail.com>
2024-03-27 09:52:03 +0800

    Added functionality to fetch prayer times

---
+++ musollah/api/services/fetchPrayerTimes.js
@@ -0,0 +1,16 @@
+export const fetchPrayerTimes = async (city, date) => {
+    try {
+        const response = await fetch(`http://api.aladhan.com/v1/timingsByCity?city=Singapore&country=Singapore&method=8`)
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch prayer times');
+        }
+
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error('Error fetching prayer times: ', error);
+        throw error;
+    }
+}
--- musollah/screens/PrayerTimeScreen.js
+++ musollah/screens/PrayerTimeScreen.js
@@ -1,10 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { fetchPrayerTimes } from '../api/services/fetchPrayerTimes'
 
 const PrayerTimeScreen = () => {
+  const [prayerTimes, setPrayerTimes] = useState(null);
+
+  useEffect(() => {
+    const fetchPrayerTimesData = async () => {
+      try {
+        const data = await fetchPrayerTimes();
+        setPrayerTimes(data);
+      } catch (error) {
+        console.log(error)
+      }
+    };
+
+    fetchPrayerTimesData();
+  }, [])
+
   return (
-    <View>
-      <Text>PrayerTimeScreen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {prayerTimes ? (
+        <View>
+          <Text>Fajr: {prayerTimes.data.timings.Fajr}</Text>
+          <Text>Dhuhr: {prayerTimes.data.timings.Dhuhr}</Text>
+          <Text>Asr: {prayerTimes.data.timings.Asr}</Text>
+          <Text>Maghrib: {prayerTimes.data.timings.Maghrib}</Text>
+          <Text>Isha: {prayerTimes.data.timings.Isha}</Text>
+        </View>
+      ) : (
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      )}
     </View>
   )
 }
