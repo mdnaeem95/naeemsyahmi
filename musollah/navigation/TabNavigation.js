2a34003227f5ecb0c84b9246e76f16bd28e18e39
Naeem Sani <muhdnaeem95@gmail.com>
2024-04-13 09:11:26 +0800

    Added icons to the navigation bar

--- musollah/navigation/TabNavigation.js
+++ musollah/navigation/TabNavigation.js
@@ -1,6 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
 
 import PrayerTimeScreen from '../screens/PrayerTimeScreen'
 import MusollahScreen from '../screens/MusollahScreen'
@@ -13,11 +15,47 @@ const Tab = createBottomTabNavigator();
 const TabNavigation = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name='Prayers' component={PrayerTimeScreen} />
-      <Tab.Screen name='Qibla' component={QiblaScreen} />
-      <Tab.Screen name='Musollah' component={MusollahScreen} />
-      <Tab.Screen name='Quran' component={QuranScreen} />
-      <Tab.Screen name='Settings' component={SettingsScreen} />
+      <Tab.Screen 
+        name='Prayers' 
+        component={PrayerTimeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name={'person-praying'} size={20} />
+          )
+        }} />
+      <Tab.Screen 
+        name='Qibla' 
+        component={QiblaScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name={'compass'} size={20} />
+          )
+        }} />
+      <Tab.Screen 
+        name='Musollah' 
+        component={MusollahScreen} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name={'mosque'} size={20} />
+          )
+        }}
+        />
+      <Tab.Screen 
+        name='Quran' 
+        component={QuranScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name={'book-quran'} size={20} />
+          )
+        }} />
+      <Tab.Screen 
+        name='Settings' 
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name={'gear'} size={20} />
+          )
+        }} />
     </Tab.Navigator>
   )
 }
